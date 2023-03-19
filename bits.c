673642a6e51c824bc1966e75c74bd042b39455b5
Chenxu Han <10225101440@stu.ecnu.edu.cn>
2023-03-19 06:18:40 +0000

    Complete puzzle 1 and 2

--- bits.c
+++ bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return ~(x&y)&~(~x&~y);
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -153,7 +153,7 @@ int bitXor(int x, int y) {
  */
 int tmin(void) {
 
-  return 2;
+  return 1<<31;
 
 }
 //2
