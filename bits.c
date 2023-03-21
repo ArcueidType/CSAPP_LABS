31e1daac1bb3066ea40c995d87864984fff92700
Chenxu Han <10225101440@stu.ecnu.edu.cn>
2023-03-21 11:55:01 +0000

    solve puzzle 9

--- bits.c
+++ bits.c
@@ -243,7 +243,7 @@ int isLessOrEqual(int x, int y) {
  *   Rating: 4 
  */
 int logicalNeg(int x) {
-  return 2;
+  return (x>>31|(~x+1)>>31)+1;
 }
 /* howManyBits - return the minimum number of bits required to represent x in
  *             two's complement
