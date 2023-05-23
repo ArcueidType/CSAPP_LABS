8c73b04c4f457c44d8a210068669a7b5b79ebca6
Chenxu Han <10225101440@stu.ecnu.edu.cn>
2023-05-23 15:36:14 +0000

    Update to ver 1.1

--- .ipynb_checkpoints/README-checkpoint.md
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -1,5 +1,7 @@
 # Update Log:
 
+## Latest version: 1.1
+
 ## 05/19/23 
 
 1. Initialize "project".
@@ -19,3 +21,13 @@
 2. Complete function updateTime.
 
 3. Get the correct answer, ver 1.0 is online, waiting to be added more details.
+
+## 05/23/23
+
+1. Optimize code structure.
+
+2. Implement -h command, which used to show help info.
+
+3. Implement -v command, which used to show verbose info while working.
+
+4. New version 1.1 is now available.
--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -14,76 +14,99 @@ int countHits = 0;
 int countMisses = 0;
 int countEvictions = 0;
 int s, E, b;
+int verboseFlag = 0;
+int isModifying = 0;
 
 typedef struct cacheLine{
     int bitValid;
     unsigned long tag;
     int time;
-} cacheLine;
+} cacheLine, *cacheSet, **cache;
 
-//void helpUsage(void);
+void helpUsage(void){
+    printf("Usage: ./csim [-hv] -s <s> -E <E> -b <b> -t <tracefile>\n");
+    printf("-h: Print help info.\n");
+    printf("-v: Print verbose info.\n");
+    printf("-s <num>: Number of setIndex bits.\n");
+    printf("-E <num>: Number of cache lines per set.\n");
+    printf("-b <num>: Number of block offset bits.\n");
+    printf("-t <file>: Path of trace file.\n");
+}
 
-cacheLine **allocateCache(int s, int E, int b){
+cache allocateCache(int s, int E, int b){
     int setNum = 1<<s;
-    cacheLine **cache;
-    cache = (cacheLine **)malloc(setNum*sizeof(cacheLine *));
+    cache myCache;
+    myCache = (cache)malloc(setNum*sizeof(cacheSet));
     for(int i=0;i<setNum;i++){
-	cache[i]=(cacheLine *)malloc(E*sizeof(cacheLine));
+	myCache[i]=(cacheSet)malloc(E*sizeof(cacheLine));
     }
     for(int i=0;i<setNum;i++){
 	for(int j=0;j<E;j++){
-	    cache[i][j].bitValid = 0;
-	    cache[i][j].tag = -1lu;
-	    cache[i][j].time = 0;
+	    myCache[i][j].bitValid = 0;
+	    myCache[i][j].tag = -1lu;
+	    myCache[i][j].time = 0;
 	}
     }
-    return cache;
+    return myCache;
 }
 
-void update(unsigned long address, cacheLine **cache){
+void loadOrStore(unsigned long address, char op, int size, cache myCache){
     int newSetIndex = (address>>b) % (1<<s);
     unsigned long newTag = address>>(s+b);
+    if(verboseFlag && !isModifying) printf("%c %lx,%d", op, address, size);
     for(int i=0;i<E;i++){
-	if(cache[newSetIndex][i].tag==newTag && cache[newSetIndex][i].bitValid==1){
+	if(myCache[newSetIndex][i].tag==newTag && myCache[newSetIndex][i].bitValid==1){
+	    if(verboseFlag) printf(" hit");
 	    countHits++;
-	    cache[newSetIndex][i].time = 0;
+	    myCache[newSetIndex][i].time = 0;
 	    return;
 	}
     }
 
     countMisses++;
+    if(verboseFlag) printf(" miss");
     for(int i=0;i<E;i++){
-	if(cache[newSetIndex][i].bitValid==0){
-	    cache[newSetIndex][i].bitValid = 1;
-	    cache[newSetIndex][i].tag = newTag;
-	    cache[newSetIndex][i].time = 0;
+	if(myCache[newSetIndex][i].bitValid==0){
+	    myCache[newSetIndex][i].bitValid = 1;
+	    myCache[newSetIndex][i].tag = newTag;
+	    myCache[newSetIndex][i].time = 0;
 	    return;
 	}
     }
     
     int evictLine=0;
     countEvictions++;
+    if(verboseFlag) printf(" eviction");
     for(int i=0;i<E;i++){
-	if(cache[newSetIndex][i].time > cache[newSetIndex][evictLine].time){
+	if(myCache[newSetIndex][i].time > myCache[newSetIndex][evictLine].time){
 	    evictLine = i;
 	}
     }
-    cache[newSetIndex][evictLine].tag = newTag;
-    cache[newSetIndex][evictLine].time = 0;
+    myCache[newSetIndex][evictLine].tag = newTag;
+    myCache[newSetIndex][evictLine].time = 0;
     return;
 }
 
-void updateTime(cacheLine **cache){
+void modify(unsigned long address, int size, cache myCache){
+    if(verboseFlag) printf("M %lx,%d", address, size);
+    isModifying = 1;
+    loadOrStore(address, 'L', size, myCache);
+    loadOrStore(address, 'S', size, myCache);
+    isModifying = 0;
+    return;
+}
+
+void updateTime(cache myCache){
     int setNum = 1<<s;
     for(int i=0;i<setNum;i++){
 	for(int j=0;j<E;j++){
-	    if(cache[i][j].bitValid == 1) cache[i][j].time++;
+	    if(myCache[i][j].bitValid == 1) myCache[i][j].time++;
 	}
     }
     return;
 }
 
-void handleTrace(FILE *fp, cacheLine **cache){
+void handleTrace(FILE *fp, cache myCache){
     char opt;
     unsigned long int addr;
     int size;
@@ -92,17 +115,19 @@ void handleTrace(FILE *fp, cacheLine **cache){
 	    case 'I':
 		break;
 	    case 'M':
-		update(addr, cache);
-		update(addr, cache);
+		modify(addr, size, myCache);
+		printf("\n");
 		break;
 	    case 'L':
-		update(addr, cache);
+		loadOrStore(addr, 'L', size, myCache);
+		printf("\n");
 		break;
 	    case 'S':
-		update(addr, cache);
+		loadOrStore(addr, 'S', size, myCache);
+		printf("\n");
 		break;
 	}
-        updateTime(cache);
+        updateTime(myCache);
     }
     return;
 }
@@ -111,12 +136,14 @@ int main(int argc, char *argv[])
 {
     FILE *fp=NULL;
     int opt;
+    int helpUsageFlag=0;
     while((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1){
 	switch(opt){
 	    case 'h':
-		//helpUsage();
+		helpUsageFlag=1;
 		break;
 	    case 'v':
+		verboseFlag=1;
 		break;
 	    case 's':
 		s = atoi(optarg);
@@ -132,10 +159,14 @@ int main(int argc, char *argv[])
 		break;
 	}		
     }
-    cacheLine **cache = allocateCache(s, E, b);
-    handleTrace(fp, cache);
-    for(int i=0;i<(1<<s);i++) free(cache[i]);
-    free(cache);
+    if(helpUsageFlag==1 || s<=0 || E<=0 || b<=0 || fp==NULL){
+	helpUsage();
+	return 0;
+    }
+    cache myCache = allocateCache(s, E, b);
+    handleTrace(fp, myCache);
+    for(int i=0;i<(1<<s);i++) free(myCache[i]);
+    free(myCache);
     fclose(fp);
     printSummary(countHits, countMisses, countEvictions);
     return 0;
